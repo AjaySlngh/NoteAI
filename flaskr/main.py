cde55b5688a734db6e378c831af6922ad445c557
Ajay Singh <aos2500@g.rit.edu>
2023-09-23 18:23:15 -0400

    did more with upload page

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -12,9 +12,9 @@ def open_front_page():
 def route_ata():
     return redirect('https://ata.mroley.dev')
 
-@app.route('/recording')
-def send_to_recording():
-    return render_template('recording.html')
-
-
-
+@app.route('/uploading', methods=['GET', 'POST'])
+def send_to_uploading():
+    if request.method == 'POST':
+        print(request.form.listvalues())
+        
+    return render_template('upload.html')

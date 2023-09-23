c09ec0c8222316ef58f19d9174d802a34259dba1
Ajay Singh <aos2500@g.rit.edu>
2023-09-23 15:54:51 -0400

    working flask website

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask import redirect, url_for, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def open_front_page():
+    return render_template('frontPage.html', name='Ata')
+
+@app.route('/ata')
+def route_ata():
+    return redirect('https://ata.mroley.dev')
+
+@app.route('/recording')
+def send_to_recording():
+    return render_template('recording.html')
+
+
 
